022a291b55043bc196f9ef69aa3a5676e353dd97
keqishou <keqishou@qq.com>
2016-11-01 15:58:59 +0800

    dd

--- hello-world/src/com/github/keqishou/helloworld/Test.java
+++ hello-world/src/com/github/keqishou/helloworld/Test.java
@@ -4,4 +4,7 @@ package com.github.keqishou.helloworld;
  * Created by Administrator on 2016/11/1.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("welcome here, hello world");
+    }
 }
